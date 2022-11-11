b3e13f4b705c9ac5c3db38c58ce9a7a5141b6eb5
Franco Aguilera <flaalf@gmail.com>
2022-11-11 19:55:36 -0300

    the guessing game

---
+++ src/03_the_guessing_game/guess_the_number/src/main.rs
@@ -0,0 +1,34 @@
+use colored::*;
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    let mut rng = rand::thread_rng();
+    let secret_number = rng.gen_range(0..101);
+
+    println!("=================The Guessing Game=================");
+    println!("Enter your number guess (0..100):");
+
+    loop {
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Faild to read line!");
+
+        let guess: u8 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("{}", "Too small".red()),
+            Ordering::Greater => println!("{}", "Too big".red()),
+            Ordering::Equal => {
+                println!("{}", "You win :)".green());
+                println!("The secret number was: {}", secret_number);
+                break;
+            }
+        }
+    }
+}
